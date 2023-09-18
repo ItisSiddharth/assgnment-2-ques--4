1fb567e3b5aed724459aa6d1e908276f1fbad7b8
sidharth <sidharthkumar@gmail.com>
2023-09-18 14:39:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.setNameAndPassword("pathak", 1234);
+            Logger.Login("pathak", 1234);
+
+        }
+    }
+
+    public static class Logger
+    {
+        static string name;
+        static int password;
+        public static void setNameAndPassword(string username, int userpassword)
+        {
+            name = username;
+            password = userpassword;
+        }
+        public static void Login(string username, int userpassword)
+        {
+            if (username == name && userpassword == password)
+            {
+                Console.WriteLine("Login SuccessFull");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Login Failed");
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
